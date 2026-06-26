20657d3225a3f85ebc85b6deebdcec46d08c6db3
Simon Wang <wang6712@purdue.edu>
2026-06-26 13:18:32 -0400

    Rewrite preview logic

--- Assets/Scripts/Objects/CollabObjectPreview.cs
+++ Assets/Scripts/Objects/CollabObjectPreview.cs
@@ -45,6 +45,44 @@ namespace CollabXR.Objects
 			this.data = data;
 			EnableLoadingAnimation(true, data.availableOnThisPlatform);
 			LoadPrefab(data).Forget();
+
+			SetupPreviewAsync().Forget();
+		}
+
+		private async UniTaskVoid SetupPreviewAsync()
+		{
+			EnableLoadingAnimation(true, true);
+
+			await UniTask.WaitUntil(() => ModManager.Instance.indexedMods.ContainsKey(data.modGUID));
+
+			while (loadingBar.fillAmount < 1 && RequestExists(out UnityWebRequest request))
+			{
+				if (request.result == UnityWebRequest.Result.Success || request.result == UnityWebRequest.Result.InProgress)
+				{
+					loadingBar.fillAmount = request.downloadProgress;
+				}
+				else
+				{
+					EnableLoadingAnimation(true, false);
+					break;
+				}
+
+				await UniTask.Yield();
+			}
+		}
+
+		private bool RequestExists(out UnityWebRequest request)
+		{
+			request = ModManager.Instance.TryGetUnityWebRequest(data.modGUID);
+			if (request != null)
+			{
+				ModLoadTask task = new(data.modGUID);
+				ModManager.Instance.LoadMod(task);
+
+				return false;
+			}
+
+			return true;
 		}
 
 		public async UniTaskVoid LoadPrefab(CollabObjectData data)
@@ -77,36 +115,7 @@ namespace CollabXR.Objects
 
 		private void Update()
 		{
-			if (state == PreviewState.Loading && data != null)
-			{
-				if (ModManager.Instance.indexedMods.ContainsKey(data.modGUID))
-				{
-					
-					UnityWebRequest request = ModManager.Instance.TryGetUnityWebRequest(data.modGUID);
-					if (request != null)
-					{
-						bool successful_progress = request.result == UnityWebRequest.Result.Success || request.result == UnityWebRequest.Result.InProgress;
-						if (!successful_progress)
-						{
-							EnableLoadingAnimation(true, false);
-						}
-						else
-						{
-							//EnableLoadingAnimation(true, true);
-							loadingBar.fillAmount = request.downloadProgress;
-						}
-					}
-					else
-					{
-						ModLoadTask task = new(data.modGUID);
-						ModManager.Instance.LoadMod(task);
-					}
-				}
-				else
-				{
-					EnableLoadingAnimation(true, true);
-				}
-			}
+			
 		}
 
 		private void OnDestroy()
