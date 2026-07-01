210e720375677c3628914e337a8020a44bb113de
Simon Wang <wang6712@purdue.edu>
2026-07-01 15:05:26 -0400

    Fixed an issue where clearing cache in room breaks all existing objects

--- Assets/Scripts/Development/ClearCache.cs
+++ Assets/Scripts/Development/ClearCache.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace CollabXR.Development
 {
 	public class ClearCache : MonoBehaviour
 	{
-		public void DebugClearCache()
+		// Toggled false while in room, will break object loading otherwise.
+		public void DebugClearCache(bool unloadAllObjects)
 		{
-			AssetBundle.UnloadAllAssetBundles(true);
+			AssetBundle.UnloadAllAssetBundles(unloadAllObjects);
 			Debug.Log($"Attempting to clear cache, result: {Caching.ClearCache()}");
 		}
 	}
